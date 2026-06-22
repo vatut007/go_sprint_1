358248c1c23643633c55615b3616b970a9b650e2
Ilya <sov16@ya.ru>
2026-06-22 11:31:31 +0300

    lesson 8 3

--- lesson_8/sale_auto/cmd/main.go
+++ lesson_8/sale_auto/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
@@ -36,6 +37,17 @@ func carFunc(id string) string {
 	return "unknown identifier " + id
 }
 
+func carBrandHandle(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte(chi.URLParam(r, "brand")))
+}
+
+func carBrandModelHandle(rw http.ResponseWriter, r *http.Request) {
+	brand := chi.URLParam(r, "brand")
+	model := chi.URLParam(r, "model")
+	final := fmt.Sprintf("%s %s", brand, model)
+	rw.Write([]byte(final))
+}
+
 func carsHandle(rw http.ResponseWriter, r *http.Request) {
 	carsList := carsListFunc()
 	io.WriteString(rw, strings.Join(carsList, ", "))
@@ -49,5 +61,13 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/", carsHandle)
 	r.Get("/car/{id}", carHandle)
+	r.Route("/cars", func(r chi.Router) {
+		r.Get("/", carsHandle)
+		r.Route("/{brand}", func(r chi.Router) {
+			r.Get("/", carBrandHandle)
+			r.Get("/{model}", carBrandModelHandle)
+		})
+	})
+	// r.Get("/cars/{brand}", carBrandHandle)
 	http.ListenAndServe(":8080", r)
 }
